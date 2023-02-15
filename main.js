3d49b17bbaad2e5096fe32ebbde0765b267ccd88
Petra Ulicna <petra.ulicna@hotmail.com>
2023-02-15 22:16:28 +0100

    final

---
+++ main.js
@@ -0,0 +1,19 @@
+/*
+let title = 'Avengers'
+
+title.length
+title.toUpperCase()
+title.slice(0,5)
+title.slice(title.length - 5, title.length)
+*/
+
+let email = prompt(String('Zadejte emailou adresu:'))
+let atIndex = email.indexOf('@')
+
+let nameDomain = {
+    name: email.slice(0,atIndex),
+    domain: email.slice(atIndex + 1, email.lenght),
+}
+
+document.body.innerHTML = "<p> User name: " + nameDomain.name + "</p>" + "<p> Domain: " + nameDomain.domain + "</p>"
+
