111193af5351f2a45e451f0e7b8f98f22fe7212b
Petra Ulicna <petra.ulicna@hotmail.com>
2023-02-16 18:01:10 +0100

    Correction done

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ title.slice(0,5)
 title.slice(title.length - 5, title.length)
 */
 
-let email = prompt(String('Zadejte emailou adresu:'))
+let email = prompt('Zadejte emailou adresu:')
 let atIndex = email.indexOf('@')
 
 let nameDomain = {
     name: email.slice(0,atIndex),
-    domain: email.slice(atIndex + 1, email.lenght),
+    domain: email.slice(atIndex + 1, email.length),
 }
 
 document.body.innerHTML = "<p> User name: " + nameDomain.name + "</p>" + "<p> Domain: " + nameDomain.domain + "</p>"
